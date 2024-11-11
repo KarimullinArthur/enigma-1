a6dbc61b51c9d9c0c2c8df87985f6513af84aeba
Karimullin Arthur <KarimullinArthur@disroot.org>
2024-11-11 05:56:15 +0300

    Ops

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import base64
 class Enigma:
     MAX_CHR = 1114111
 
-    roter_1 = 31
-    roter_2 = 55
+    roter_1 = 0
+    roter_2 = 0
     roter1_start = roter_1
     roter2_start = roter_2
     
@@ -14,8 +14,8 @@ class Enigma:
     roter_2_com = {}
 
     for c in range(0, MAX_CHR):
-        roter_1_com[c] = c
-        roter_2_com[c] = c
+        roter_1_com[c] = c 
+        roter_2_com[c] = c 
 
     roter_1_com_inv = dict(zip(roter_1_com.values(), roter_1_com.keys()))
     roter_2_com_inv = dict(zip(roter_2_com.values(), roter_1_com.keys()))
@@ -36,8 +36,7 @@ class Enigma:
                 print('PING')
                 self.roter_1 = 0
                 self.roter_2 = 0
-                ch = (self.roter_1_com[char_code]+self.roter_1+self.roter_2)
-                result.append(chr(ch))
+                result.append(chr(enc(self, char_code)))
 
             self.roter_1 += 1
             self.roter_2 += 1
@@ -72,7 +71,7 @@ class Enigma:
 
 enigma = Enigma()
 # enc = enigma.encrypt("Hello world!\nHello world!")
-_str = '''
+_str = '''000000000
 Привет бонч, ты должен УМЕРЕТЬ!!
 
  Decoding Base64 string is exactly opposite to that of encoding. First we convert the Base64 strings into unencoded data bytes followed by conversion into bytes-like object into a string. The below example depicts the decoding of the above example encode string output.
