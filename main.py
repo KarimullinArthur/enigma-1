df7e69083421c351bcce1b0ea4f5b5d52790ba2d
Karimullin Arthur <KarimullinArthur@disroot.org>
2024-11-20 02:13:33 +0300

    tmp

--- main.py
+++ main.py
@@ -2,44 +2,83 @@ import random
 import base64
 
 
+class Roter:
+    def __init__(self, number: int, position: int, turn_position: int):
+        MAX_CHR = 1114111
+        
+        self.number = number
+        self.position = position
+        self.start_position = position
+        self.turn_position = turn_position
+        self.count_of_turns = 0
+
+        self.commutation_table = {}
+
+        full_char = []
+        for c in range(0, MAX_CHR+1):
+            full_char.append(c)
+
+        for c in range(0, MAX_CHR+1):
+            self.commutation_table[c] = random.choices(full_char, k=len(full_char))
+
+        self.commutation_table_inv = dict(zip(self.commutation_table.values(), self.commutation_table.keys()))
+
+
+    def __repr__(self):
+        return f"<Roter number={self.number} position={self.position} turn_position={self.turn_position} start_position={self.start_position}>"
+    
+        
+
 class Enigma:
     MAX_CHR = 1114111
 
-    roter_1 = 0
-    roter_2 = 0
-    roter1_start = roter_1
-    roter2_start = roter_2
-    
-    roter_1_com = {}
-    roter_2_com = {}
+    def __init__(self, count_roters: int = 3):
+        self.roters = []
+        for roter_number in range(1, count_roters+1):
+            self.roters.append(Roter(number=roter_number,
+#                                      position=0,
+                                     position=random.randint(1, 10),
+                                     turn_position=random.randint(1, 3)
+#                                      turn_position=1
+                                     )
+                               )
 
-    for c in range(0, MAX_CHR):
-        roter_1_com[c] = c 
-        roter_2_com[c] = c 
+            print(self.roters)
+            print()
 
-    roter_1_com_inv = dict(zip(roter_1_com.values(), roter_1_com.keys()))
-    roter_2_com_inv = dict(zip(roter_2_com.values(), roter_1_com.keys()))
-
-
-    
     def encrypt(self, value: str):
         result = []
 
         def enc(self, char_code):
-            return self.roter_1_com[char_code]+self.roter_1+self.roter_2
+            current_code = char_code
+            for roter_number in range(len(self.roters)):
+                roter = self.roters[roter_number]
+                try:
+                    current_code = roter.commutation_table[current_code+roter.position]
+                except KeyError:
+                    if current_code+roter.position >= self.MAX_CHR:
+                        roter.position = self.MAX_CHR - current_code
+#                         print(current_code)
+#                         print(roter.position)
+                    current_code = roter.commutation_table[current_code+roter.position]
+
+                roter.position += 1
+                roter.count_of_turns += 1
+                if roter.count_of_turns > roter.turn_position:
+#                     print("PING")
+#                     print(roter.count_of_turns)
+#                     print(roter.turn_position)
+                    roter.count_of_turns = 0
+                    try:
+                        self.roters[roter_number+1].position += 1
+                    except IndexError:
+                        pass
+            return current_code
 
         for char in value:
             char_code = ord(char)
-            try:
-                result.append(chr(enc(self, char_code)))
-            except ValueError:
-                print('PING')
-                self.roter_1 = 0
-                self.roter_2 = 0
-                result.append(chr(enc(self, char_code)))
-
-            self.roter_1 += 1
-            self.roter_2 += 1
+            result.append(chr(enc(self, char_code)))
+                
         result = ''.join(result).encode()
         result = base64.b64encode(result)
         return result.decode()
@@ -49,29 +88,34 @@ class Enigma:
         value = base64.b64decode(value.encode()).decode()
         print()
         print(value)
-        str(value)
+        print()
+        for ch in value:
+            print(ord(ch), end=' ')
         print()
 
-        def enc(self):
+        def dec(self):
+#             result = self.roter_2_com[self.roter_2 - self.roter2_start]
+#             result = self.roter_1_com[self.roter_1 - result]
+#             return result
             return self.roter_1_com_inv[ord(char)]-self.roter1_start-self.roter2_start
 
         for char in value:
             try:
-                result.append(chr(enc(self)))
+                result.append(chr(dec(self)))
             except ValueError:
                 print('PINGD', end='')
                 self.roter1_start = 0
                 self.roter2_start = 0
-                result.append(chr(enc(self)))
+                result.append(chr(dec(self)))
 
             self.roter1_start += 1
             self.roter2_start += 1
         return ''.join(result)
 
 
-enigma = Enigma()
+enigma = Enigma(count_roters=1)
 # enc = enigma.encrypt("Hello world!\nHello world!")
-_str = '''000000000
+_str = '''00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
 Привет бонч, ты должен УМЕРЕТЬ!!
 
  Decoding Base64 string is exactly opposite to that of encoding. First we convert the Base64 strings into unencoded data bytes followed by conversion into bytes-like object into a string. The below example depicts the decoding of the above example encode string output.
@@ -83,5 +127,25 @@ Example:'''
 
 
 enc = enigma.encrypt(_str)
+print("Inp chars")
+for ch in _str:
+    print(ord(ch), end=' ')
+print()
+print("Encrypt:")
 print(enc)
+print()
+dec_str = base64.b64decode(enc).decode()
+for ch in range(len(dec_str)):
+    print(dec_str[ch], end='')
+print()
+print()
+for ch in range(len(dec_str)):
+    print((ord(dec_str[ch]) - ord(dec_str[ch-1])), end=" ")
+print()
+print()
+for ch in range(len(dec_str)):
+    print((ord(dec_str[ch]), ord(dec_str[ch]) - ord(dec_str[ch-1])), end=' ')
+print()
+print()
+print("Decrypt:")
 print(enigma.decrypt(enc))
